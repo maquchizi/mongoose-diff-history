045d5226e8a7ebdd6c66333335cc0c1f7e49111e
Mark Ng'ang'a <maqnganga@gmail.com>
2017-12-19 01:07:26 +0300

    Make user an reference to the User model

--- diffHistoryModel.js
+++ diffHistoryModel.js
@@ -4,7 +4,7 @@ var historySchema = new mongoose.Schema(
         collectionName: {type: String},
         collectionId: {type: mongoose.Schema.Types.ObjectId},
         diff: {},
-        user: {},
+        user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
         reason: {type: String},
         version: {type: Number, min: 0}
     },
